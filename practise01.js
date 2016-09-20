50d747e91ca910cdf1ca6ec327ef184efe4f3d4a
amber <15981971556@163.com>
2016-09-20 11:03:05 +0800

    Update practise01.js
    两种排序方法，一种直接用 js中sort()方法，一种是直接双循环冒泡排序。

--- practise01.js
+++ practise01.js
@@ -1,15 +1,44 @@
+ /**
+maxData()返回给定整数数组的元素组成的最大值
+	用sort方法降序
+*/
 var maxData = function(a){
- 	a.sort(
-		function(e1,e2){
-			return e2-e1;
-		}
-	);// 数组降序 
-	//console.log(a);
-	for(var i = 0;i<a.length;i++){
-		var s = String(a[i]);// 类型转换  
-		var str = a.join('');// 连接字符 
-	}
-	return str;		    	
+   a.sort(
+    function(e1,e2){
+    	return e2-e1;
+    }
+    );
+    console.log(a);
+    for(var i = 0;i<a.length;i++){
+    	var s = String(a[i]);
+    	var str = a.join('');
+    }
+    return str;		    	
 }
-var b = maxData([9,5,6]); 连接字符 
-console.log(b);
+var result1 = maxData([9,5,6]);
+console.log(result1);
+
+/**
+	用冒泡方法降序
+*/
+var maxData = function(a){
+	console.log(a);
+	for(var i =0;i<a.length;i++){
+	    for(var j = 0;j<a.length-i-1;j++){
+	    	if(a[j]<a[j+1]){
+	    		var tmp = 0;
+	    		tmp = a[j];
+	    		a[j] = a[j+1];
+	    		a[j+1] = tmp;
+	    	}
+	    }
+	}
+	for(var i = 0;i<a.length;i++){
+	    var s = String(a[i]);
+	    var str = a.join('');
+	}
+	return str;
+}
+var result2 = maxData([2,5,6]);
+console.log(result2);
+//console.log(str);
